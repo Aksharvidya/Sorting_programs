55a4d782915ed984a4f40a4c15d6f1be1924cb0a
BhoomikaM2006 <bhoomikamalavade2006@gmail.com>
2026-03-29 11:54:04 +0530

    created another version of insertion in python

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,16 +1,19 @@
 def insertion_sort(arr):
+    # Loop over the array starting from the second element
     for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-
-        # Move elements greater than key one position ahead
+        key = arr[i]  # The element to be inserted into the sorted portion
+        j = i - 1  # Start comparing with the element before
+        
+        # Shift elements of arr[0..i-1], that are greater than key, to one position ahead
         while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
             j -= 1
-
+        
+        # Place the key after the last shifted element
         arr[j + 1] = key
+    
+    return arr
 
-# Example
-arr = [5, 2, 9, 1, 3]
-insertion_sort(arr)
-print(arr)
+# Example usage
+arr = [12, 11, 13, 5, 6]
+print("Sorted array:", insertion_sort(arr))
